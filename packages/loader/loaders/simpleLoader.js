705eb0ab899df25cb22f318031328e9e8166d15c
GaryHjy <782316959@qq.com>
2021-08-13 11:21:06 +0800

    feat: 添加loader package

---
+++ packages/loader/loaders/simpleLoader.js
@@ -0,0 +1,7 @@
+
+function simpleLoader(content, map, meta) {
+  console.log("我是 simpleLoader")
+  return content;
+}
+
+module.exports = simpleLoader;
---
+++ packages/loader/src/index.js
@@ -0,0 +1,13 @@
+import data from './data.txt';
+
+const text = 123;
+
+function showText () {
+  return text;
+}
+
+console.log(data)
+
+export {
+  showText
+}
